fa249b7914f8560b685b54ff9a8f660eecfecbd8
kbub27 <kalebmerriman27@gmail.com>
2019-03-13 16:26:57 -0700

    html structure and variables in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // CREATE VARIABLES FOR THE IMAGES
+    var crystal1 = $("<img>");
+    var crystal2 = $("<img>");
+    var crystal3 = $("<img>");
+
+    
+
+
+
+})
